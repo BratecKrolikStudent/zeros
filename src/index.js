ce5513a4a674df6f38cfbe29a45c773dd5584576
Dmitriy Kunets <t38@tut.by>
2018-09-11 19:47:07 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+function makeBertrana(num) {
+  let number = num, n = 1;
+
+  return function getBertrana(result = 0) {
+    let d = Math.pow(5, n++);
+    return d <= number ? getBertrana(result += (number / d) | 0) : result;
+  }
+}
+
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let bertrana = makeBertrana(number)
+  return bertrana();
 }
